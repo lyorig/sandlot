42ae31a6b12147620ffb916b6d5d8b3e63094d72
Petr Šácha <petrsacha@proton.me>
2026-09-09 19:14:24 +0200

    docs: clipboard + msgbox + pixels

--- src/msgbox.rs
+++ src/msgbox.rs
@@ -1,3 +1,20 @@
+//! SDL offers a simple message box API, which is useful for simple alerts,
+//! such as informing the user when something fatal happens at startup without
+//! the need to build a UI for it (or informing the user _before_ your UI is
+//! ready).
+//!
+//! These message boxes are native system dialogs where possible.
+//!
+//! There is both a customizable function (`SDL_ShowMessageBox`) that offers
+//! lots of options for what to display and reports on what choice the user
+//! made, and also a much-simplified version ([`show`]), which merely takes a
+//! text message and title, and waits until the user presses a single "OK" UI
+//! button. Often, this is all that is necessary.
+//!
+//! Implementation checklist ([source](https://wiki.libsdl.org/SDL3/CategoryMessagebox)):
+//! - [ ] SDL_ShowMessageBox
+//! - [x] SDL_ShowSimpleMessageBox
+
 use std::ffi::CStr;
 
 use sdl3_sys::messagebox::*;
--- src/pixels.rs
+++ src/pixels.rs
@@ -1,4 +1,99 @@
 //! Various enums related to pixels and/or graphics.
+//!
+//! SDL offers facilities for pixel management.
+//!
+//! Largely these facilities deal with pixel _format_: what does this set of
+//! bits represent?
+//!
+//! If you mostly want to think of a pixel as some combination of red, green,
+//! blue, and maybe alpha intensities, this is all pretty straightforward, and
+//! in many cases, is enough information to build a perfectly fine game.
+//!
+//! However, the actual definition of a pixel is more complex than that:
+//!
+//! Pixels are a representation of a color in a particular color space.
+//!
+//! The first characteristic of a color space is the color type. SDL
+//! understands two different color types, RGB and YCbCr, or in SDL also
+//! referred to as YUV.
+//!
+//! RGB colors consist of red, green, and blue channels of color that are added
+//! together to represent the colors we see on the screen.
+//!
+//! <https://en.wikipedia.org/wiki/RGB_color_model>
+//!
+//! YCbCr colors represent colors as a Y luma brightness component and red and
+//! blue chroma color offsets. This color representation takes advantage of
+//! the fact that the human eye is more sensitive to brightness than the
+//! color in an image. The Cb and Cr components are often compressed and have
+//! lower resolution than the luma component.
+//!
+//! <https://en.wikipedia.org/wiki/YCbCr>
+//!
+//! When the color information in YCbCr is compressed, the Y pixels are left
+//! at full resolution and each Cr and Cb pixel represents an average of the
+//! color information in a block of Y pixels. The chroma location determines
+//! where in that block of pixels the color information is coming from.
+//!
+//! The color range defines how much of the pixel to use when converting a
+//! pixel into a color on the display. When the full color range is used, the
+//! entire numeric range of the pixel bits is significant. When narrow color
+//! range is used, for historical reasons, the pixel uses only a portion of
+//! the numeric range to represent colors.
+//!
+//! The color primaries and white point are a definition of the colors in the
+//! color space relative to the standard XYZ color space.
+//!
+//! <https://en.wikipedia.org/wiki/CIE_1931_color_space>
+//!
+//! The transfer characteristic, or opto-electrical transfer function (OETF),
+//! is the way a color is converted from mathematically linear space into a
+//! non-linear output signals.
+//!
+//! <https://en.wikipedia.org/wiki/Rec._709#Transfer_characteristics>
+//!
+//! The matrix coefficients are used to convert between YCbCr and RGB colors.
+//!
+//! Blend modes decide how two colors will mix together. There are both
+//! standard modes for basic needs and a means to create custom modes
+//! ([`BlendMode::compose`]), dictating what sort of math to do on what color
+//! components.
+//!
+//! Implementation checklist:
+//!
+//! From [CategoryPixels](https://wiki.libsdl.org/SDL3/CategoryPixels):
+//! - [x] SDL_PixelFormat
+//! - [x] SDL_Colorspace
+//! - [ ] SDL_ArrayOrder
+//! - [ ] SDL_BitmapOrder
+//! - [ ] SDL_ChromaLocation
+//! - [ ] SDL_ColorPrimaries
+//! - [ ] SDL_ColorRange
+//! - [ ] SDL_ColorType
+//! - [ ] SDL_MatrixCoefficients
+//! - [ ] SDL_PackedLayout
+//! - [ ] SDL_PackedOrder
+//! - [ ] SDL_PixelType
+//! - [ ] SDL_TransferCharacteristics
+//! - [ ] SDL_CreatePalette
+//! - [ ] SDL_DestroyPalette
+//! - [ ] SDL_GetMasksForPixelFormat
+//! - [ ] SDL_GetPixelFormatDetails
+//! - [ ] SDL_GetPixelFormatForMasks
+//! - [ ] SDL_GetPixelFormatName
+//! - [ ] SDL_GetRGB
+//! - [ ] SDL_GetRGBA
+//! - [ ] SDL_MapRGB
+//! - [ ] SDL_MapRGBA
+//!
+//! `SDL_MapSurfaceRGB` and `SDL_MapSurfaceRGBA` are covered by the
+//! [surface](crate::surface) module, and so is `SDL_ScaleMode`.
+//!
+//! From [CategoryBlendmode](https://wiki.libsdl.org/SDL3/CategoryBlendmode):
+//! - [x] SDL_BlendMode
+//! - [x] SDL_BlendFactor
+//! - [x] SDL_BlendOperation
+//! - [x] SDL_ComposeCustomBlendMode
 
 use sdl3_sys::{blendmode::*, pixels::*, surface::SDL_ScaleMode};
 
